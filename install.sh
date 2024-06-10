59d8361be67c30c40a6b1d3acf59f731a07b6abe
phazoxNL <59491722+phazoxNL@users.noreply.github.com>
2024-06-10 02:02:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ fi
 
 # Always remove lib.sh, before downloading it
 rm -rf /tmp/lib.sh
-curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
+curl -sSL -o /tmp/lib.sh https://raw.githubusercontent.com/phazoxNL/pterodactyl-installer/master/lib/lib.sh
 # shellcheck source=lib/lib.sh
 source /tmp/lib.sh
 
